eca409311b20ee2950ab018d5ac42b9e745f8a2c
YaroslavKuzero <49403383+YaroslavKuzero@users.noreply.github.com>
2020-07-16 17:01:59 +0300

    Updates

---
+++ precache-manifest.1153e7ef531693e04280d065f67f410f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63f5cce61cee7fc7c3f4b2ba2af8d81",
+    "url": "/goit-react-hw-03-phonebook2/index.html"
+  },
+  {
+    "revision": "a8574107bda6bde06093",
+    "url": "/goit-react-hw-03-phonebook2/static/css/main.a3ab7cb4.chunk.css"
+  },
+  {
+    "revision": "563e6fe70992bf5aeecc",
+    "url": "/goit-react-hw-03-phonebook2/static/js/2.f62864ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook2/static/js/2.f62864ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8574107bda6bde06093",
+    "url": "/goit-react-hw-03-phonebook2/static/js/main.aa139ffa.chunk.js"
+  },
+  {
+    "revision": "f56c4c8d622cb41b44fb",
+    "url": "/goit-react-hw-03-phonebook2/static/js/runtime-main.be1ae74d.js"
+  }
+]);
